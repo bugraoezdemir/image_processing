aa34ce9fbb65714677f6f39cc9a2879df828b4dd
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2021-11-05 17:18:04 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
     url = 'https://github.com/bugraoezdemir/image_processing'     
     ,
     # license = 'MIT',
-    packages = setuptools.find_packages(
+    packages = setuptools.find_packages()
     # install_requires = ['requests'],
     )
