23f63aa3e5422c3ef3a0da793d00d9a389330c7d
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2021-11-05 16:08:00 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 14:57:57 2021
+
+@author: bugra
+"""
+
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name = 'image_processing',
+    version = '0.0.0',
+    author = 'Bugra Özdemir',
+    author_email = 'bugraa.ozdemir@gmail.com',
+    description = 'Image processing tools.',
+    long_description = 'Tools for the processing of 3D microscopy images.',
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/bugraoezdemir/image_processing'     
+    ,
+    # license = 'MIT',
+    packages = ['wrappers', 'visualisation', 'utils', 'transforms'],
+    # install_requires = ['requests'],
+    )
