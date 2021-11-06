30e963537a840d55169ec528894309aa6e84c6d2
bugraoezdemir <91956472+bugraoezdemir@users.noreply.github.com>
2021-11-06 15:18:01 +0100

    Add files via upload

--- image_processing/transforms/photometric/local_filtering/simple_itk_filters.py
+++ image_processing/transforms/photometric/local_filtering/simple_itk_filters.py
@@ -7,5 +7,15 @@ Created on Thu Mar 26 18:36:12 2020
 
 
 
-from ....wrappers.simple_itk_filters import *
-    
+try:
+    from image_processing.wrappers import __init__
+    wrappers_exists = True
+except:
+    wrappers_exists = False
+    print('The simple_itk_filters subpackage is currently not available\
+     because the "wrappers" package is not yet included in the pip installation.')
+    print('This issue will soon be solved as there will be a separate installation\
+     recipe for the "wrappers" package.')
+
+if wrappers_exists:
+    from image_processing.transforms.photometric.local_filtering.simple_itk_filters import *
